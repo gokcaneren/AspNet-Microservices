e87ad12194a61dc4a2d7a99db331d0f721a25cee
gokcaneren <gokcann01@gmail.com>
2022-09-22 11:37:52 +0300

    host extension added

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ordering.Infrastructure.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,9 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host=CreateHostBuilder(args).Build();
+            host.MigrateDatabase<OrderContext>();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
